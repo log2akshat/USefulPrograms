b7acaff9a913bdb8368fae660671b838276b9ff6
akshat <akshat-pg8@iiitmk.ac.in>
2023-04-24 01:51:22 +0530

    printer_pages.py: Program to output n numbers of
    pages to print to feed on the printer menu when
    two pages are selected to print on a paper.

---
+++ python/printer_pages.py
@@ -0,0 +1,25 @@
+# !/bin/python
+
+# Program to output n numbers of pages to print to feed on the
+# printer menu when 2 pages are selected to print on a paper.
+
+import itertools
+
+first_page_number=int(input("Enter First Page Number: "))
+print(first_page_number)
+second_page_number=int(input("Enter Second Page Number: "))
+print(second_page_number)
+last_page_number=int(input("Enter Last Page Number: "))
+print(last_page_number)
+
+INC_VALUE=4
+
+list1=list(range(first_page_number, last_page_number, INC_VALUE))
+list2=list(range(second_page_number, last_page_number, INC_VALUE))
+new_list=[]
+for a,b in itertools.zip_longest(list1,list2):
+    if a:
+        new_list.append(a)
+    if b:
+        new_list.append(b)
+print(new_list)

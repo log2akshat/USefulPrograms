7513db36edd51954e4d8ea2a02bc6534d31203df
akshat <akshat-pg8@iiitmk.ac.in>
2023-11-23 15:26:55 +0530

    Removed unnecessaru packages

--- java/SpringBoot Microservices/ms_spring_order/src/main/java/com/ms/order/service/OrderServiceImpl.java
+++ java/SpringBoot Microservices/ms_spring_order/src/main/java/com/ms/order/service/OrderServiceImpl.java
@@ -3,7 +3,6 @@ package com.ms.order.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ms.order.entity.OrderRegisterEntity;

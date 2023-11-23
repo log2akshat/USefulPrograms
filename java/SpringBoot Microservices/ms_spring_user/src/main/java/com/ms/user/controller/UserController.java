24b8dd8a6c3c7133113d13ed354d4b6ccb238e4e
akshat <akshat-pg8@iiitmk.ac.in>
2023-11-24 03:43:19 +0530

    Removed unnecessary imports

--- java/SpringBoot Microservices/ms_spring_user/src/main/java/com/ms/user/controller/UserController.java
+++ java/SpringBoot Microservices/ms_spring_user/src/main/java/com/ms/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.ms.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- java/SpringBoot Microservices/ms_spring_user/src/main/java/com/ms/user/service/UserServiceImpl.java
+++ java/SpringBoot Microservices/ms_spring_user/src/main/java/com/ms/user/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.ms.user.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ms.user.entity.UserRegisterEntity;

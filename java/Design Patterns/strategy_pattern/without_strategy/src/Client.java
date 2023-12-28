70cb48553ed91ec5e5702532e8e97129edead4d2
akshat <akshat-pg8@iiitmk.ac.in>
2023-12-28 19:20:28 +0530

    strategy_pattern: Code without using Strategy Pattern
    Here, there is a Vehicle class which provides the drive
    capability to the inhering classes. Here, SportsVehicle,
    OffRoadVehicle and NormalVehicle extends the Vehicle class.
    
    Problem:
    We can observe from the code that SportsVehicle and the
    OffRoadVehicle both needs SportsVehicle drive capability
    so both the classes have the code duplicated the same code.
    
    So, multiple child classes inherit from a common parent,
    but some child classes share similar behavior without having
    a direct relationship with each other.
    
    To avoid code duplication in these unrelated child classes,
    the Strategy Design Pattern can be applied.

---
+++ java/Design Patterns/strategy_pattern/without_strategy/src/Client.java
@@ -0,0 +1,14 @@
+public class Client {
+
+    public static void main(String[] args) {
+        Vehicle vehicle1 = new PassengerVehicle();
+        vehicle1.drive();
+
+        Vehicle vehicle2 = new SportsVehicle();
+        vehicle2.drive();
+
+        Vehicle vehicle3 = new OffRoadVehicle();
+        vehicle3.drive();
+    }
+    
+}

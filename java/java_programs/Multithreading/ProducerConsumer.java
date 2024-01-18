2e7536c78d0e79c859e7430e957569dd84d5d4e0
akshat <akshat-pg8@iiitmk.ac.in>
2024-01-18 17:44:58 +0530

    Minor improvements

--- java/java_programs/Multithreading/ProducerConsumer.java
+++ java/java_programs/Multithreading/ProducerConsumer.java
@@ -3,26 +3,24 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 
 class Q {
-    //int n;
+    // Double Ended Queue: Deque where you can insert and remove elements from both ends of the queue.
+    //BlockingQueue<Integer> queue = new LinkedBlockingDeque<>();
     BlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
-    //BlockingQueue<Integer> queue = new LinkedBlockingDeque<>(); //Double Ended Queue: Deque where you can insert and remove elements from both ends of the queue.
     boolean valueSet = false;
 
-    synchronized int get() {
-        //System.out.println("Inside get()");
+    synchronized void get() {
         while(!valueSet && queue.size()<=7) {
             try {
-                wait(); // 2. Causes execution to suspend until Producer notifies that some data is ready. When this happens execution inside get() resumes.
+                // 2. Causes execution to suspend until Producer notifies that some data is ready.
+                //    When this happens execution inside get() resumes.
+                wait();
             } catch (InterruptedException e) {}
         }
-        //System.out.println("Consumer Thread Got: " + n);
-        System.out.println("Consumer Thread Processing: " + queue);
-        //System.out.println("Size of the Queue: " + queue.size());
-        System.out.println("Consumer Thread Got: " + queue.element());
+        System.out.println("Consumer Thread Get: " + queue);
+        System.out.println("Consumer Thread Processing: " + queue.remove());
         valueSet = false;
-        notify(); // 4. After obtaining data, notify() tells Producer that it is okay to put more data in the Queue.
-        //return n;
-        return queue.remove();
+        // 4. After obtaining data, notify() tells Producer that it is okay to put more data in the Queue.
+        notify();
     }
 
     synchronized void put(BlockingQueue<Integer> q) {
@@ -30,22 +28,24 @@ class Q {
         while(valueSet) {
             try {
                 Thread.sleep(1000);
-                wait(); // 3. Suspends execution until Consumer has removed the item from the Queue.
+                // 3. Suspends execution until Consumer has removed the item from the Queue.
+                wait();
             } catch (InterruptedException e) {}
         }
-        //this.n = n;
         this.queue = q;
         valueSet = true;
         System.out.println("Producer Thread Put: " + q);
-        notify(); // 1. When execution resumes, the next item of the data is put into the Queue and notify() is called. This tells Consumer to consume the data.
+        // 1. When execution resumes, the next item of the data is put into the Queue and notify() is called.
+        //    This tells Consumer to consume the data.
+        notify();
     }
 }
 
 class Producer implements Runnable {
     Q q;
     Thread t;
-    BlockingQueue<Integer> queue = new LinkedBlockingQueue<>(10);
     //BlockingQueue<Integer> queue = new LinkedBlockingDeque<>(10);
+    BlockingQueue<Integer> queue = new LinkedBlockingQueue<>(10);
 
     Producer(Q q) {
         this.q = q;
